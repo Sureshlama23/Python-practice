b204b8008b151fc4e8eb1b0056619614d7cbffee
Suresh <sureshlama812@gmail.com>
2023-11-23 22:12:13 +0545

    shuffle of deck cards program

---
+++ shuffle of deck cards.py
@@ -0,0 +1,8 @@
+"""shuffle of deck card programming"""
+import random, itertools
+
+deck = list(itertools.product(range(1,14),["Spade","club","Hearts","Diamond"]))
+random.shuffle(deck)
+
+for i in range(5):
+    print(deck[i][0],'of', deck[i][1])
